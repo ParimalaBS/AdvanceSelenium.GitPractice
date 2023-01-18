1ebaa875e4fc633306720e69b9e6600245cc61c7
Shivunayak <Shivunayak@Shivu>
2023-01-18 19:57:13 +0530

    commit new code

---
+++ src/test/java/samplepractice/AdvanceJava.java
@@ -0,0 +1,14 @@
+package samplepractice;
+
+import org.testng.annotations.Test;
+
+public class AdvanceJava {
+	
+	@Test
+	public void practiceTest() {
+		
+		System.out.println("practice");
+		
+	}
+
+}
