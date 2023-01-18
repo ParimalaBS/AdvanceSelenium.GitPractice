9e385eddf338d036fdd3236fa33967b456214807
Shivunayak <Shivunayak@Shivu>
2023-01-18 13:41:40 +0530

    new commit and push

---
+++ src/test/java/demopractice/CreateContactTest.java
@@ -0,0 +1,14 @@
+package demopractice;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("test passed");
+	}
+
+}
