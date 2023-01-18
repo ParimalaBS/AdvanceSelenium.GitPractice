b9438e5cf6f4b0b2ad26092dbb764032f76d95a0
Shivunayak <Shivunayak@Shivu>
2023-01-18 18:36:04 +0530

    commit From Lead

---
+++ src/test/java/demopractice/CreateContactWithOrg.java
@@ -0,0 +1,15 @@
+package demopractice;
+
+import org.testng.annotations.Test;
+
+public class CreateContactWithOrg {
+	
+	@Test
+	public void orgName() {
+		
+		System.out.println("Infosys");
+	}
+	
+	
+
+}
