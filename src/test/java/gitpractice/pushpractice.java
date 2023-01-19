89fead7d0ce6eab1c5b20355a8e73cc4134b8cb0
Shivunayak <Shivunayak@Shivu>
2023-01-19 19:52:48 +0530

    code commited

---
+++ src/test/java/gitpractice/pushpractice.java
@@ -0,0 +1,14 @@
+package gitpractice;
+
+import org.testng.annotations.Test;
+
+public class pushpractice {
+	
+	
+	@Test
+	public void pushCode() {
+		
+		System.out.println("code pushed");
+	}
+
+}
